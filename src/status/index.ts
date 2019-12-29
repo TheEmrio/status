765004b6ff030b04d5f61b3b2a0646df765e6617
TheEmrio <emrio@emrio.fr>
2019-12-29 02:29:52 +0100

    added status fetcher

---
+++ src/status/index.ts
@@ -0,0 +1,46 @@
+import pm2 from 'pm2'
+import moment from 'moment'
+import debug from 'debug'
+import config from '../config'
+const log = debug('status:status')
+
+export interface ServiceStatus {
+  name: string
+  status: string
+  uptime: string
+}
+
+const cache = {
+  status: [] as ServiceStatus[]
+}
+
+function getServiceStatuses (): Promise<ServiceStatus[]> {
+  return new Promise((resolve, reject) => {
+    pm2.list((err, processes) => {
+      if (err) return reject(err)
+      const statuses = processes.map(p => ({
+        name: p.name || 'N/A',
+        status: (p.pm2_env || {}).status || 'N/A',
+        uptime: moment((p.pm2_env || {}).pm_uptime).fromNow(true)
+      }))
+      return resolve(statuses)
+    })
+  })
+}
+
+async function update (): Promise<void> {
+  log('Updating service data...')
+  cache.status = await getServiceStatuses()
+}
+
+export async function getStatuses (): Promise<ServiceStatus[]> {
+  return cache.status
+}
+
+pm2.connect(err => {
+  if (err) {
+    console.error(err)
+    process.exit(2)
+  }
+  setInterval(update, config.updateInterval)
+})
